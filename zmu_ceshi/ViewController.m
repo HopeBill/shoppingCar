2e9d91f9026956a4c2dafb65c4b5f4e00ceda514
1255418023@qq.com <1255418023@qq.com>
2017-12-22 10:41:50 +0800

    初始化

---
+++ zmu_ceshi/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  zmu_ceshi
+//
+//  Created by lee on 2017/12/22.
+//  Copyright © 2017年 Libiyong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
